1c96afdd7df4ead7b3b3c6c681bd15fb23a62122
yh <a@b.com>
2021-07-14 15:31:02 +0900

    Insert実装 ・大枠だけ

--- app/src/main/java/jp/codingkakapo/forgetcheck/view/EditItemFragment.kt
+++ app/src/main/java/jp/codingkakapo/forgetcheck/view/EditItemFragment.kt
@@ -1,5 +1,6 @@
 package jp.codingkakapo.forgetcheck.view
 
+import android.app.Application
 import android.os.Bundle
 import android.text.Editable
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import jp.codingkakapo.forgetcheck.ForgetCheckApplication
 import jp.codingkakapo.forgetcheck.R
 import jp.codingkakapo.forgetcheck.databinding.FragmentChecklistBinding
 import jp.codingkakapo.forgetcheck.databinding.FragmentEditItemBinding
@@ -14,7 +16,7 @@ import jp.codingkakapo.forgetcheck.viewModel.EditItemViewModel
 
 class EditItemFragment : Fragment() {
 
-    private val vm = EditItemViewModel()
+    private lateinit var vm : EditItemViewModel
     private lateinit var binding : FragmentEditItemBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +26,11 @@ class EditItemFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        //VMいれる
+        vm = EditItemViewModel(this.context?.applicationContext as ForgetCheckApplication)
+
         // binding初期化
         binding = FragmentEditItemBinding.inflate(inflater, container, false)
 
@@ -39,12 +45,18 @@ class EditItemFragment : Fragment() {
         // テキスト入力欄フォーカスイベ登録
         binding.editText.setOnFocusChangeListener { view, hasFocus ->
             if (hasFocus) /* hasFocusがT・・・入力開始時nothing to do */
-            else vm.hogeText.value = (view as? EditText)?.text.toString()
+            else {
+                val str = (view as? EditText)?.text.toString()
+                vm.hogeText.value = str
+                vm.updatedStrings.value = str
+            }
         }
 
         // ボタン保存イベ登録
         binding.button.setOnClickListener {
-            vm.hogeText.value = binding.editText.text.toString()
+            val str = binding.editText.text.toString()
+            vm.hogeText.value = str
+            vm.updatedStrings.value = str
         }
         return binding.root
     }
--- app/src/main/java/jp/codingkakapo/forgetcheck/viewModel/EditItemViewModel.kt
+++ app/src/main/java/jp/codingkakapo/forgetcheck/viewModel/EditItemViewModel.kt
@@ -1,15 +1,40 @@
 package jp.codingkakapo.forgetcheck.viewModel
 
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import jp.codingkakapo.forgetcheck.ForgetCheckApplication
+import jp.codingkakapo.forgetcheck.model.AnxietyModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
 
-class EditItemViewModel : ViewModel() {
+class EditItemViewModel(app : ForgetCheckApplication) : AndroidViewModel(app) {
+
     val hogeText : MutableLiveData<String> by lazy {
         MutableLiveData<String>("HELLO WORLD")
     }
+
+    // Editになんか入力されて変更反映されるべき時点で呼ばれる
+    val updatedStrings : MutableLiveData<String> = MutableLiveData()
+
     init{
-        hogeText.observeForever({
-        })
+        // 確認用の表示
+        hogeText.observeForever{}
+
+        // 変更時イベント、　DBにInsert
+        updatedStrings.observeForever {
+            viewModelScope.launch {
+                insertAnxieties(app,it)
+            }
+        }
+    }
+
+    private suspend fun insertAnxieties(app : ForgetCheckApplication, text : String){
+         withContext(Dispatchers.IO){
+             app.DB.AnxietyDao().insert(
+                 AnxietyModel(0, text, LocalDateTime.now(), LocalDateTime.now(), false)
+             )
+         }
     }
 }
